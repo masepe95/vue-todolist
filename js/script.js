604ace77012cf4030bcb6d884aa7b3289439d2e1
masepe95 <marcosepe10@gmail.com>
2023-06-20 21:57:04 +0200

    Vue ready to perform

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 console.log('JS OK' Vue) 
 
+const app = Vue.createApp({
+    name: 'Todolist',
+    data() {
+        return {};
+    },
+
+    computed: {
+
+    },
+
+    methods: {
+
+    }
+
+})
+
+
+app.mount('#root');
